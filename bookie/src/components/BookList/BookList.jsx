fc9afe11ff1308c5c76748d838adda9940cc3fa8
Yasuko Miyazawa <yasukomiyazawa08@gmail.com>
2023-09-07 00:47:49 +0900

    added book list section

--- bookie/src/components/BookList/BookList.jsx
+++ bookie/src/components/BookList/BookList.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import CoverImg from "../../images/coverNF.jpg";
+import Book from "../BookList/Book";
+import { useGlobalContext } from "../../content";
+import Loading from "../Loader/Loader";
+import "./BookList.css";
+
+// https://covers.openlibrary.org/b/id/240727-S.jpg
 
 const BookList = () => {
-  return <div>BookList</div>;
+  const { books, loading, resultTitle } = useGlobalContext();
+  const booksWithCovers = books.map((singleBook) => {
+    return {
+      ...singleBook,
+      id: singleBook.id.replace("/works/", ""),
+      cover_img: singleBook.cover_id
+        ? `https://covers.openlibrary.org/b/id/${singleBook.cover_id}-L.jpg`
+        : CoverImg,
+    };
+  });
+
+  if (loading) return <Loading />;
+
+  return (
+    <section className="booklist">
+      <div className="container">
+        <div className="section section-title">
+          <h2>{resultTitle}</h2>
+        </div>
+        <div className="booklist-content grid">
+          {booksWithCovers.slice(0, 30).map((item, index) => {
+            return <Book key={index} {...item} />;
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default BookList;
--- bookie/src/components/SearchForm/SearchForm.jsx
+++ bookie/src/components/SearchForm/SearchForm.jsx
@@ -1,25 +1,48 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+import { useGlobalContext } from "../../content";
 import "./SearchForm.css";
 
 const SearchForm = () => {
+  const { setSearchTerm, setResultTitle } = useGlobalContext();
+  const searchText = useRef("");
+  const navigate = useNavigate();
+
+  useEffect(() => searchText.current.focus(), []);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let tempSearchTerm = searchText.current.value.trim();
+    if (tempSearchTerm.replace(/[^\w\s]/gi, "").length === 0) {
+      setSearchTerm("The Alchemist");
+      setResultTitle("Please Enter Something ...");
+    } else {
+      setSearchTerm(searchText.current.value);
+    }
+    navigate("/book");
+  };
+
   return (
     <div className="search-form">
       <div className="conatiner">
         <div className="search-form-content">
-          <div className="search-form">
+          <form className="search-form" onSubmit={handleSubmit}>
             <div className="search-from-elem flex flex-sb bg-white">
               <input
                 type="text"
                 className="form-control"
                 placeholder="The Alchemist .."
+                ref={searchText}
               />
-              <button type="submit" className="flex flex-c">
+              <button
+                type="submit"
+                className="flex flex-c"
+                onClick={handleSubmit}
+              >
                 <FaSearch className="text-maroon size={32}" />
               </button>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
