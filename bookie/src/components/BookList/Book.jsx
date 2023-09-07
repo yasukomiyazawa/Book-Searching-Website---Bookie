b39f43654c78bbea66b66f0b0245e8555f05701d
Yasuko Miyazawa <yasukomiyazawa08@gmail.com>
2023-09-07 22:46:45 +0900

    added css styling to BookList section

--- bookie/src/components/BookList/Book.jsx
+++ bookie/src/components/BookList/Book.jsx
@@ -10,19 +10,22 @@ const Book = (book) => {
       </div>
       <div className="book-item-info text-center">
         <Link to={`/book/${book.id}`} {...book}>
-          <div className="book-item-info-item fw-7 fs-18">
+          <div className="book-item-info-item title fw-7 fs-18">
             <span>{book.title}</span>
           </div>
         </Link>
-        <div className="book-item-info author fs-15">
+
+        <div className="book-item-info-item author fs-15">
           <span className="text-capitalize fw-7">Author: </span>
-          <span>{book.author.join(",")}</span>
+          <span>{book.author.join(", ")}</span>
         </div>
-        <div className="book-item-info edition-count fs-15">
-          <span className="text-capitalize fw-7">Total Edition: </span>
+
+        <div className="book-item-info-item edition-count fs-15">
+          <span className="text-capitalize fw-7">Total Editions: </span>
           <span>{book.edition_count}</span>
         </div>
-        <div className="book-item-info publish-year fs-15">
+
+        <div className="book-item-info-item publish-year fs-15">
           <span className="text-capitalize fw-7">First Publish Year: </span>
           <span>{book.first_publish_year}</span>
         </div>
